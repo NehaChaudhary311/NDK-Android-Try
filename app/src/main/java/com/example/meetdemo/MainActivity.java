12b00cd9f591d01eea14b2e1750603786271e03f
Neha <nehavchaudhry@gmial.com>
2021-02-03 13:33:48 +0530

    emulator problem

--- app/src/main/java/com/example/meetdemo/MainActivity.java
+++ app/src/main/java/com/example/meetdemo/MainActivity.java
@@ -20,14 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        //TextView tv = findViewById(R.id.sample_text);
-        //tv.setText(stringFromJNI());
+        TextView tv = findViewById(R.id.discoverable);
+
 
         //Code for switch - make me discoverable
         Switch makeDiscoverable = (Switch)findViewById(R.id.makeDiscoverable);
         Boolean switchState = makeDiscoverable.isChecked();
         if(switchState == true) {
-            Toast.makeText(getApplicationContext(),stringForMakeDiscoverable(),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(),stringForMakeDiscoverable(),Toast.LENGTH_SHORT).show();
+            tv.setText(stringForMakeDiscoverable());
         }
         else{
             Toast.makeText(getApplicationContext(),"You're not discoverable now",Toast.LENGTH_SHORT).show();
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
+
     public native String stringFromJNI();
     public native String stringForMakeDiscoverable();
 }
